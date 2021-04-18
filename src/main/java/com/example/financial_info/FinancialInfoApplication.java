b950b0d6c10d29396c8b47fc9de3219700baefec
ChenYan <2547113514@qq.com>
2021-04-18 13:14:29 +0800

    test

---
+++ src/main/java/com/example/financial_info/FinancialInfoApplication.java
@@ -0,0 +1,13 @@
+package com.example.financial_info;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialInfoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinancialInfoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/financial_info/FinancialInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.financial_info;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialInfoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
